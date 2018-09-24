1263297985fbd728a737f9d4c61fc723f44a98b9
Mathias Brugger <voldardard@web01.robinsons.ch>
2018-09-24 14:58:53 +0200

    Add migrations

--- database/migrations/2018_09_10_135742_order.php
+++ database/migrations/2018_09_10_135742_order.php
@@ -13,7 +13,19 @@ class Order extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('order', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->timestamps();
+            $table->nullableTimestamps('paid_date');
+            $table->unsignedInteger('fk_status_id');
+            $table->foreign('fk_status_id')->references('id')->on('status');
+            $table->string('name', 60);
+            $table->string('fsname', 60);
+            $table->string('adress', 100);
+            $table->string('npa', 6);
+            $table->string('city', 40);
+            $table->email('email', 60);
+        });
     }
 
     /**
