0083ec853c09daf369c5bc3d54e10481b0e2ecb3
Mathias Brugger <brugger.mathias@hotmail.com>
2018-10-01 10:41:12 +0200

    configure models

--- app/order.php
+++ app/order.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
 
 class order extends Model
 {
-    //
+
+    use Notifiable;
+
+    /**
+     *  Table
+     *
+     *
+     */
+    protected $table = 'order';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'fsname', 'paid_date', 'address', 'npa', 'city', 'email'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
 }
