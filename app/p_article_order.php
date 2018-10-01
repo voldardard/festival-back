5009e860ab5995455c070826f00e645a30eaa6a6
Mathias Brugger <voldardard@web01.robinsons.ch>
2018-10-01 11:22:14 +0200

    add model

---
+++ app/p_article_order.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class p_article_order extends Model
+{
+    //
+}
---
+++ app/p_size_article.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class p_size_article extends Model
+{
+    //
+}
